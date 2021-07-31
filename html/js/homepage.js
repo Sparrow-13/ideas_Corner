f5c484cce6981ef40c7a03dd752524c944131e86
Adnan Qureshi <72442243+Sparrow-13@users.noreply.github.com>
2021-07-31 20:08:23 +0530

    homepage.js uploaded

---
+++ html/js/homepage.js
@@ -0,0 +1,15 @@
+function drop() {
+  let targetDiv = document.getElementById("drop_down");
+  let el = document.getElementsByClassName('main_background');
+  let imag = document.getElementById("down_image");
+  if (targetDiv.style.display !== "none") {
+    targetDiv.style.display = "none";
+    imag.src="assets/caret-down-fill.svg";
+    el.style="backdrop-filter: blur(6px);";
+
+  } else {
+    targetDiv.style.display = "block";
+    imag.src="assets/caret-up-fill.svg";
+    
+  }
+};
